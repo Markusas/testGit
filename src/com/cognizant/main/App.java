bcf5e998be8fa77969748be6049082aaff21497a
Markusas <saltinio@gmail.com>
2021-12-16 15:36:05 +0200

    Create Person Create home page

--- src/com/cognizant/main/App.java
+++ src/com/cognizant/main/App.java
@@ -1,7 +1,12 @@
 package com.cognizant.main;
 
+import com.cognizant.bean.Person;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Person person = new Person();
+        person.sayHello("Marek");
     }
 }
